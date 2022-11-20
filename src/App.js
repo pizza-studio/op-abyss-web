b088d31df82af81d2fb552c4fc5dc730581931e3
Bill-Haku <battleandwar@hotmail.com>
2022-11-20 22:31:00 +0800

    feat: add table

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import useSWR from "swr";
 
 // const fetcher = config => axios(config).then(res => res.data);
 const fetcher = async url => {
-  const res = await axios.get(url,{
+  const res = await axios.get(url, {
     headers: {
       'Access-Control-Allow-Origin': '*'
     }
   }
-    );
+  );
   if (res.retCode) {
     const error = new Error('An error occurred while fetching the data.')
     // 将额外的信息附加到错误对象上。
@@ -31,16 +31,50 @@ function App() {
 
   if (!data) return <div>loading...</div>
 
+  var table = "";
+
+  for (var chars in data.avatars) {
+    table += "<tr><td>" + data.avatars[chars].charId + "</td><td>" + data.avatars[chars].percentage + "</td></tr>";
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>原神披萨深渊榜</h1>
         <p>Powered by Genshin Pizza Helper</p>
 
-        <h2>总用户：{data.totalUsers}</h2>
+        <h3>总用户：{data.totalUsers}</h3>
+
+        <table>
+          <thead>
+            <tr>
+              <th>角色</th>
+              <th>使用率</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              getTable(data.avatars)
+            }
+          </tbody>
+        </table>
       </header>
     </div>
   );
 }
 
 export default App;
+
+function getTable(data) {
+  const rowData = [];
+  data.forEach(element => {
+    rowData.push(<tr><td>{element.charId}</td><td>{toPercent(element.percentage)}</td></tr>);
+  })
+  return rowData;
+}
+
+function toPercent(point){
+  var str=Number(point*100).toFixed(2);
+  str+="%";
+  return str;
+}
